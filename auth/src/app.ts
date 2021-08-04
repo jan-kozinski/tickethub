b7c0e38b7bdd51733d0efd068c1c2bd745159c72
Jan Kozinski <dzejdzej9753@gmail.com>
2021-08-04 16:31:52 +0200

    changed base client axios client url and enabled cookies usage via http instead of https

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -21,7 +21,7 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== "test",
+    secure: false,
   })
 );
 
--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -4,8 +4,7 @@ const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     //We're on the server!
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://www.dzony12-tickets.website",
       headers: req.headers,
     });
   } else {
